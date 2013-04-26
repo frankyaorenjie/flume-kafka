0a7b5ee1928906bcefe9b8c240c7904910aa9b71
frankyao <baniu.yao@gmail.com>
2013-04-26 17:26:14 +0800

    update jar

--- src/main/java/com/vipshop/flume/sink/kafka/KafkaSink.java
+++ src/main/java/com/vipshop/flume/sink/kafka/KafkaSink.java
@@ -36,12 +36,11 @@ public class KafkaSink extends AbstractSink implements Configurable{
 				log.info("----------------------send");
 				producer.send(new ProducerData<String, String>(this.topic, e.getBody().toString()));
 				tx.commit();
-				return Status.READY；
+				return Status.READY;
 			} catch(Exception ex) {
 				log.info("------------------------------------", ex);
 				throw ex;
 			}
-			return Status.READY;
 		} catch(Exception e) {
 			tx.rollback();
 			log.info("---------------------------------", e);
