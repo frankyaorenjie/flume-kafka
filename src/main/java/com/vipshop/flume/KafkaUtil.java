f0e34905487aad8b0e60702b26997790bc4526e1
frankyao <frankyao@frankyao-OptiPlex-3010.(none)>
2013-05-20 16:33:59 +0800

    update log4j

--- src/main/java/com/vipshop/flume/KafkaUtil.java
+++ src/main/java/com/vipshop/flume/KafkaUtil.java
@@ -12,11 +12,8 @@ import org.apache.flume.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.vipshop.flume.sink.kafka.KafkaSink;
-
-
 public class KafkaUtil {
-	private static final Logger log = LoggerFactory.getLogger(KafkaSink.class);
+	private static final Logger log = LoggerFactory.getLogger(KafkaUtil.class);
 	/**
 	 * @param args
 	 */
