c028c249e1e59cddd43b522d19283b31cf31c537
frank.yao <baniu.yao@gmail.com>
2013-11-07 14:15:36 +0800

    update Producer Params

--- src/main/java/com/vipshop/flume/KafkaProducerConstants.java
+++ src/main/java/com/vipshop/flume/KafkaProducerConstants.java
@@ -5,10 +5,20 @@ public class KafkaProducerConstants {
 	/**
 	 * @param args
 	 */
-	public final static String CONFIG_ZK_CONNECT = "zkconnect";
-	public final static String CONFIG_ZK_TIMEOUT = "zktimeout";
-	public final static String CONFIG_TOPIC = "topic";
-	public final static String CONFIG_BATCH_SIZE = "batchsize";
-	public final static String CONFIG_GROUP = "group";
-	public final static String CONFIG_RESET_OFFSET = "resetoffset";
+	public final static String CONFIG_PRODUCER_TYPE = "producer.type";
+	public final static String CONFIG_BROKER_LIST = "broker.list";
+	public final static String CONFIG_ZK_CONNECT = "zk.connect";
+	public final static String CONFIG_BUFFER_SIZE = "buffer.size";
+	public final static String CONFIG_CONNECT_TIMEOUT_MS = "connect.timeout.ms";
+	public final static String CONFIG_SOCKET_TIMEOUT_MS = "socket.timeout.ms";
+	public final static String CONFIG_RECONNECT_INTERVAL = "reconnect.interval";
+	public final static String CONFIG_RECONNECT_TIME_INTERVAL_MS = "reconnect.time.interval.ms";
+	public final static String CONFIG_MAX_MESSAGE_SIZE = "max.message.size";
+	public final static String CONFIG_COMPRESSION_CODEC = "compression.codec";
+	public final static String CONFIG_COMPRESSED_TOPICS = "compressed.topics";
+	public final static String CONFIG_ZK_READ_NUM_RETRIES = "zk.read.num.retries";
+	// Options for Async Producers
+	public final static String CONFIG_QUEUE_TIME = "queue.time";
+	public final static String CONFIG_QUEUE_SIZE = "queue.size";
+	public final static String CONFIG_BATCH_SIZE = "batch.size";
 }
