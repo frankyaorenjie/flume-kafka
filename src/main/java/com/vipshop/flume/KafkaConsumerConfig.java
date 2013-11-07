0b412d49a7c98a6ca2426c5e5c7317141aa99ded
frank.yao <baniu.yao@gmail.com>
2013-11-07 12:23:26 +0800

    update Consumer Constatns and its default value

--- src/main/java/com/vipshop/flume/KafkaConsumerConfig.java
+++ src/main/java/com/vipshop/flume/KafkaConsumerConfig.java
@@ -4,46 +4,46 @@ import org.apache.flume.Context;
 
 public class KafkaConsumerConfig {
 	public static String getGroupId(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_GROUPID, "groupid");
 	}
 	public static String getSocketTimeoutMs(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_SOCKET_TIMEOUT_MS, "30000");
 	}
 	public static String getSocketBufferSize(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_SOCKET_BUFFER_SIZE, "65536"); // 64*1024
 	}
 	public static String getFetchSize(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_FETCH_SIZE, "307200"); // 300*1024
 	}
 	public static String getBackOffIncrementMs(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_BACKOFF_INCREMENT_MS, "1000");
 	}
 	public static String getQueuedChunksMax(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_QUEUEDCHUNKS_MAX, "100");
 	}
 	public static String getAutoCommitEnable(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_AUTOCOMMIT_ENABLE, "true");
 	}
 	public static String getAutoCommitIntervalMs(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_AUTOCOMMIT_INTERVAL_MS, "10000");
 	}
 	public static String getAutoOffsetReset(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_AUTOOFFSET_RESET, "smallest");
 	}
 	public static String getConsumerTimeoutMs(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_CONSUMER_TIMEOUT_MS, "-1");
 	}
 	public static String getRebalanceRetriesMax(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_REBALANCE_RETRIES_MAX, "4");
 	}
 	public static String getMirrorTopicsWhiteList(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_MIRROR_TOPICS_WHITELIST, "");
 	}
 	public static String getMirrorTopicsBlackList(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_MIRROR_TOPICS_BLACKLIST, "");
 	}
 	public static String getMirrorConsumerNumThreads(Context context) {
-		return context.getString(KafkaProducerConstants.CONFIG_ZK_CONNECT);
+		return context.getString(KafkaConsumerConstants.CONFIG_MIRROR_CONSUMER_NUMTHREADS, "4");
 	}
 
 }
--- src/main/java/com/vipshop/flume/KafkaConsumerConstants.java
+++ src/main/java/com/vipshop/flume/KafkaConsumerConstants.java
@@ -5,10 +5,18 @@ public class KafkaConsumerConstants {
 	/**
 	 * @param args
 	 */
-	public final static String CONFIG_ZK_CONNECT = "zkconnect";
-	public final static String CONFIG_ZK_TIMEOUT = "zktimeout";
-	public final static String CONFIG_TOPIC = "topic";
-	public final static String CONFIG_BATCH_SIZE = "batchsize";
-	public final static String CONFIG_GROUP = "group";
-	public final static String CONFIG_RESET_OFFSET = "resetoffset";
+	public final static String CONFIG_GROUPID = "groupid";
+	public final static String CONFIG_SOCKET_TIMEOUT_MS = "socket.timeout.ms";
+	public final static String CONFIG_SOCKET_BUFFER_SIZE = "socket.buffersize"; 
+	public final static String CONFIG_FETCH_SIZE = "fetch.size"; 
+	public final static String CONFIG_BACKOFF_INCREMENT_MS = "backoff.increment.ms";
+	public final static String CONFIG_QUEUEDCHUNKS_MAX = "queuedchunks.max";
+	public final static String CONFIG_AUTOCOMMIT_ENABLE = "autocommit.enable";
+	public final static String CONFIG_AUTOCOMMIT_INTERVAL_MS = "autocommit.interval.ms";
+	public final static String CONFIG_AUTOOFFSET_RESET = "autooffset.rest";
+	public final static String CONFIG_CONSUMER_TIMEOUT_MS = "consumer.timeout.ms";
+	public final static String CONFIG_REBALANCE_RETRIES_MAX = "rebalance.retries.max";
+	public final static String CONFIG_MIRROR_TOPICS_WHITELIST = "mirror.topics.whitelist";
+	public final static String CONFIG_MIRROR_TOPICS_BLACKLIST = "mirror.topics.blacklist";
+	public final static String CONFIG_MIRROR_CONSUMER_NUMTHREADS = "mirror.consumer.numthreads";
 }
