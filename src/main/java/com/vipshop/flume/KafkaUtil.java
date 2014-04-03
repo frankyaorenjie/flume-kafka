1ce4885047f5b0fe175e8caccf58b745945a1b52
李 学明 <xueming.li@dianping.com>
2014-04-03 17:00:21 +0800

    support kafka 0.8.1 and optimize getKafkaConfigProperties

--- src/main/java/com/vipshop/flume/KafkaUtil.java
+++ src/main/java/com/vipshop/flume/KafkaUtil.java
@@ -3,6 +3,11 @@ package com.vipshop.flume;
 import java.io.IOException;
 import java.util.Properties;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.UnmodifiableIterator;
+
+
 import kafka.consumer.Consumer;
 import kafka.consumer.ConsumerConfig;
 import kafka.javaapi.consumer.ConsumerConnector;
@@ -16,39 +21,42 @@ import org.slf4j.LoggerFactory;
 
 
 public class KafkaUtil {
-	private static final Logger log = LoggerFactory.getLogger(KafkaUtil.class);
-	/**
-	 * @param args
-	 */
-	public static String getKafkaConfigParameter(Context context, String key) {
-		return context.getString(key);
-	}
-	public static Properties getKafkaConfigProperties(Context context) {
-		Properties props = new Properties();
-		String contextString = context.toString();
-		for(final String kv : contextString.substring(14,contextString.length()-3).split(", ")) {
-			String k = kv.trim().split("=")[0];
-			String v = kv.trim().split("=")[1];
-			log.info("Parse Parames: " + k + "=" + v);
-			if (!k.equals("type") && !k.equals("channel")) {
-				props.put(k, v);
-			}
-		}
-		log.info("PROPS:" + props);
-		return props;
-	}
-	public static Producer<String, String> getProducer(Context context) {
-		log.info(context.toString());
-		Producer<String, String> producer;
-		producer = new Producer<String, String>(new ProducerConfig(getKafkaConfigProperties(context)));
-		return producer;
-	}
-	public static ConsumerConnector getConsumer(Context context) throws IOException, KeeperException, InterruptedException {
-		log.info(context.toString());
-		ConsumerConfig consumerConfig = new ConsumerConfig(getKafkaConfigProperties(context));
-		ConsumerConnector consumer = Consumer.createJavaConsumerConnector(consumerConfig);
-		return consumer;
-	}
+    private static final Logger log = LoggerFactory.getLogger(KafkaUtil.class);
+
+    public static String getKafkaConfigParameter(Context context, String key) {
+        return context.getString(key);
+    }
+
+    public static Properties getKafkaConfigProperties(Context context) {
+        Properties props = new Properties();
+        ImmutableMap<String, String> parameters = context.getParameters();
+        ImmutableSet<String> keys = parameters.keySet();
+        UnmodifiableIterator<String> it = keys.iterator();
+        while (it.hasNext()){
+            String k = it.next();
+            String v = parameters.get(k);
+            log.info("Parse Parames: " + k + " = " + v );
+            if (! k.equals("type") && !k.equals("channel")){
+                props.put(k, v);
+            }
+        }
+        log.info("PROPS:" + props);
+        return props;
+    }
+
+    public static Producer<String, String> getProducer(Context context) {
+        log.info(context.toString());
+        Producer<String, String> producer;
+        producer = new Producer<String, String>(new ProducerConfig(getKafkaConfigProperties(context)));
+        return producer;
+    }
+
+    public static ConsumerConnector getConsumer(Context context) throws IOException, KeeperException, InterruptedException {
+        log.info(context.toString());
+        ConsumerConfig consumerConfig = new ConsumerConfig(getKafkaConfigProperties(context));
+        ConsumerConnector consumer = Consumer.createJavaConsumerConnector(consumerConfig);
+        return consumer;
+    }
 }
 
 
